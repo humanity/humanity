49ebf3143713ee46c298a163356029feb434e200
Jack Townsend <jackmaverick1@gmail.com>
2014-02-02 16:38:04 -0500

    Work on ClientRunner

--- client/src/com/ttaylorr/dev/humanity/client/ClientRunner.java
+++ client/src/com/ttaylorr/dev/humanity/client/ClientRunner.java
@@ -1,6 +1,25 @@
 package com.ttaylorr.dev.humanity.client;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import com.ttaylorr.dev.humanity.client.packets.ClientPacketSender;
+
 public class ClientRunner {
+
+	ClientPacketSender outputSender;
+	ServerStream sstream;
+
+	static ClientRunner 
+	
+	ClientRunner(String ip, int port) throws UnknownHostException, IOException {
+		Socket sock = new Socket(ip, port);
+		sstream = new ServerStream(sock);
+		outputSender = new ClientPacketSender(sstream);
+	}
+
 	public static void main(String[] args) {
 		
 	}
