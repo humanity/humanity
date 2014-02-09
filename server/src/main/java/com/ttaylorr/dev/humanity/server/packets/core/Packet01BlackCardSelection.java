a7f27af0913769e12c19aa553c7989e187994540
Jack Townsend <jackmaverick1@gmail.com>
2014-02-08 23:58:57 -0500

    Packets go in packets.core.*, I think

--- server/src/main/java/com/ttaylorr/dev/humanity/server/packets/core/Packet01BlackCardSelection.java
+++ server/src/main/java/com/ttaylorr/dev/humanity/server/packets/core/Packet01BlackCardSelection.java
@@ -1,7 +1,8 @@
-package com.ttaylorr.dev.humanity.server.packets;
+package com.ttaylorr.dev.humanity.server.packets.core;
 
 import com.ttaylorr.dev.humanity.server.cards.core.BlackCard;
 import com.ttaylorr.dev.humanity.server.cards.core.WhiteCard;
+import com.ttaylorr.dev.humanity.server.packets.Packet;
 
 import java.util.List;
 
