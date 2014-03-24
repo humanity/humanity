98b062b346ca936adaebc0e41af529ca3dbb1f90
ttaylorr <me@ttaylorr.com>
2014-03-24 11:17:51 -0400

    Bring back that boostrap!

--- server/src/main/java/com/ttaylorr/dev/humanity/server/Bootstrap.java
+++ server/src/main/java/com/ttaylorr/dev/humanity/server/Bootstrap.java
@@ -1,17 +1,12 @@
 package com.ttaylorr.dev.humanity.server;
 
-import com.ttaylorr.dev.humanity.server.cards.factory.BlackCardFactory;
-
-import java.io.File;
-
 public class Bootstrap {
 
     static HumanityServer server = new HumanityServer(8000);
     public static final int LOOP_DELAY = 30;
 
     public static void main(String[] args) {
-        new BlackCardFactory(new File("./server/src/main/resources/cards-all.json")).parse();
-//        server.open();
-//        while(true);
+        server.open();
+        while(true);
     }
 }
