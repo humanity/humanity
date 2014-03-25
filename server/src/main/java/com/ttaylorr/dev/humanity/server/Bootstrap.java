6d69ad41f10bb4d5b1637a010bb1b3b614b81c2d
ttaylorr <me@ttaylorr.com>
2014-03-24 21:48:04 -0400

    Make proper closeRequested thingy

--- server/src/main/java/com/ttaylorr/dev/humanity/server/Bootstrap.java
+++ server/src/main/java/com/ttaylorr/dev/humanity/server/Bootstrap.java
@@ -5,8 +5,15 @@ public class Bootstrap {
     static HumanityServer server = new HumanityServer(8000);
     public static final int LOOP_DELAY = 30;
 
+    private static boolean CLOSE_REQUESTED = false;
+
     public static void main(String[] args) {
         server.open();
-        while(true);
+        while(!Bootstrap.CLOSE_REQUESTED);
+        server.close();
+    }
+
+    public static void requestClose() {
+        Bootstrap.CLOSE_REQUESTED = true;
     }
 }
