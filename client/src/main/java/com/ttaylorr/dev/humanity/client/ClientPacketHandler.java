78ed084373d682f90f6e94cdfa182e5d0a8456c2
ttaylorr <me@ttaylorr.com>
2014-03-24 21:42:36 -0400

    Register the new packets

--- client/src/main/java/com/ttaylorr/dev/humanity/client/ClientPacketHandler.java
+++ client/src/main/java/com/ttaylorr/dev/humanity/client/ClientPacketHandler.java
@@ -31,6 +31,7 @@ public class ClientPacketHandler {
         this.handlers.put(Packet05PlayerStateChange.class, new ArrayList<HandlerSnapshot>());
         this.handlers.put(Packet06HandUpdate.class, new ArrayList<HandlerSnapshot>());
         this.handlers.put(Packet07CreatePool.class, new ArrayList<HandlerSnapshot>());
+        this.handlers.put(Packet08GameChangeState.class, new ArrayList<HandlerSnapshot>());
     }
 
     public void handlePacket(Packet packet) {
